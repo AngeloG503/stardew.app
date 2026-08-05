20d1e6ff314cfdfe1b5d59b73947ea55dfd95220
AngeloG503 <ageorgiou503@gmail.com>
2026-08-05 17:30:05 +0100

    Fixed an issue where the singleton wasn't acting like a singleton for whatever reason?

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,24 +1,37 @@
 import { drizzle, type MySql2Database } from "drizzle-orm/mysql2";
-import { createPool } from "mysql2/promise";
-import { cache } from "react";
+import { createPool, type Pool } from "mysql2/promise";
 import * as schema from "./schema";
 
 export type Db = MySql2Database<typeof schema>;
 
-export const getDb = cache(() => {
-	const url = new URL(process.env.DATABASE_URL!);
+declare global {
+	// eslint-disable-next-line no-var
+	var __dbPool: Pool | undefined;
+}
 
-	const pool = createPool({
-    	host: url.hostname,
-    	port: Number(url.port),
-    	user: decodeURIComponent(url.username),
-    	password: decodeURIComponent(url.password),
-    	database: url.pathname.slice(1),
-    	connectionLimit: 10,
-	});
+function getPool(): Pool {
+	if (!global.__dbPool) {
+		const url = new URL(process.env.DATABASE_URL!);
+		global.__dbPool = createPool({
+			host: url.hostname,
+			port: Number(url.port),
+			user: decodeURIComponent(url.username),
+			password: decodeURIComponent(url.password),
+			database: url.pathname.slice(1),
+			connectionLimit: 10,
+			waitForConnections: true,
+		});
+	}
+	return global.__dbPool;
+}
 
-	return drizzle(pool, { schema, mode: "default"});
-});
+let dbInstance: Db | undefined;
+export function getDb(): Db {
+	if (!dbInstance) {
+		dbInstance = drizzle(getPool(), { schema, mode: "default" });
+	}
+	return dbInstance;
+}
 
 export async function withDb<T>(callback: (db: Db) => Promise<T>): Promise<T> {
 	return callback(getDb());
